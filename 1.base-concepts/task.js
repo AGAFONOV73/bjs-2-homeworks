a89e675b24916e5ed52b6036d36f2166c4130905
ADL13 <a.d.l.13.02.73@gmail.com>
2025-07-03 22:27:26 +0500

    Квадратное уравнение

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	const discriminant = b ** 2 - 4 * a * c;
+	let arrs = [];
+
+	if (discriminant < 0) {
+		return arrs;
+	} else if (discriminant === 0) {
+		let arr = -b / (2 * a);
+		arrs.push(arr);
+		return arrs;
+	} else {
+		const sqrtD = Math.sqrt(discriminant);
+		let arr1 = (-b + sqrtD) / (2 * a);
+		let arr2 = (-b - sqrtD) / (2 * a);
+		arrs.push(arr1, arr2);
+		return arrs;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
